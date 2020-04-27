cb2ef403ae213c6943c449282fb6df8fc4b7a17a
ziziw <ziwangchen@hotmail.com>
2020-04-26 22:57:11 -0400

    first day

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//https://drive.google.com/file/d/1Vw658-9KUiUZ5yHaABvkytC9W2QBYiW_/view
+
+document.getElementById("other-title").style.display = 'none';
+
+window.onload = () => {
+  let input = document.getElementById("name").focus();
+}
